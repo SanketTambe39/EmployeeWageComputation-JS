3c2559faeca97ee44d02191eaf310a52a139cdcf
SanketTambe39 <sankettambe39@gmail.com>
2021-08-05 21:12:28 +0530

    ADDED: code to check if employee is present or absent

--- EmployeeWageComputation.js
+++ EmployeeWageComputation.js
@@ -1,9 +1,12 @@
 {
     const IS_ABSENT = 0;
     let employeeCheck = Math.floor(Math.random() * 10) % 2;
-    if (employeeCheck == IS_ABSENT) {
+    if (employeeCheck == IS_ABSENT) 
+    {
         console.log("Employee is absent");
-    } else {
+    } 
+    else 
+    {
         console.log("Employee is present");
     }
 }

8791f2066c40ef68d6393ed1d836526a4c0922a0
SanketTambe39 <sankettambe39@gmail.com>
2021-08-08 13:39:23 +0530

    ADDED: employee gender and start date

---
+++ EmployeePayrollData.js
@@ -0,0 +1,37 @@
+class EmployeePayrollData {
+    id;
+    salary;
+    gender;
+    startDate;
+
+    constructor(...params) {
+        this.id = params[0];
+        this.name = params[1];
+        this.salary = params[2];
+        this.gender = params[3];
+        this.startDate = params[4];
+    }
+
+    get name() {
+        return this._name;
+    }
+
+    set name(name) {
+        this._name = name;
+    }
+
+    toString() {
+        const options = { year: 'numeric', month: 'long', day: 'numeric' };
+        const employeeDate = this.startDate == undefined ? "undefined" : this.startDate.toLocaleDateString("en-US", options);
+        return "ID: " + this.id + "\nName: " + this.name + "\nSalary: " + this.salary + "\nGender: " + this.gender + "\nStart date: " + this.startDate;
+    }
+}
+
+let employeePayrollData = new EmployeePayrollData(1, "Mark", 30000);
+console.log(employeePayrollData.toString());
+employeePayrollData.id = 2;
+employeePayrollData.name = "Mark";
+console.log(employeePayrollData.toString());
+
+let newEmployeePayrollData = new EmployeePayrollData(3, "Sanket", 40000, "M", new Date());
+console.log(newEmployeePayrollData.toString());
